088c7bd6b85b0fa5cfbab3f417e72220a60862c8
nikoDev <nikolas090189@gmail.com>
2025-01-11 18:50:11 -0300

    Add tabs

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -2,7 +2,8 @@ import { Link, Redirect } from "expo-router";
 import { SafeAreaView, Text, View } from "react-native";
 
 const App = () => {
-  return <Redirect href="/(stack)/home" />;
+  // return <Redirect href="/(stack)/home" />;
+  return <Redirect href="/tabs/home" />;
 
   // return (
   //   <SafeAreaView>
