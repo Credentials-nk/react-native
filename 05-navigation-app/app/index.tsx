50182e7bd0862db905063b4ccc3888ef8bec57f7
nikoDev <nikolas090189@gmail.com>
2025-01-20 19:43:23 -0300

    Fianl

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, Text, View } from "react-native";
 
 const App = () => {
   // return <Redirect href="/(stack)/home" />;
-  return <Redirect href="/drawer" />;
+  return <Redirect href="/home" />;
 
   // return (
   //   <SafeAreaView>
